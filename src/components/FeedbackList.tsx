c6c5dc3d720f57ede77383f1b3f892d259af9e99
swapnils118 <swapnil118@icloud.com>
2024-08-02 16:24:20 -0400

    Manual inputs given to nested components

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,3 +1,28 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 export default function FeedbackList() {
-  return <ol className="feedback-list">Feedback List</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>118</span>
+        </button>
+
+        <div>
+          <p>S</p>
+        </div>
+
+        <div>
+          <p>Swapnil</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur, neque repudiandae quis eos dolorum rem.
+          </p>
+        </div>
+
+        <p>1d</p>
+      </li>
+    </ol>
+  );
 }
