d4e0f3d171ebf10aae565c0ecadebd60d824d7b6
swapnils118 <swapnil118@icloud.com>
2024-08-04 15:46:15 -0400

    Working textarea and dynamic counter in form-header

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (newText.length > MAX_CHARACTERS) {
+      return;
+    }
+
+    setText(event.target.value);
+  };
+
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="adv" spellCheck={false} />
+      <textarea
+        value={text}
+        onChange={handleChange}
+        id="feedback-textarea"
+        placeholder="adv"
+        spellCheck={false}
+      />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
